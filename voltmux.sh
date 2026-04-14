663e17478e97766a169a3f5ebb0f07e02dd97265
n3k00n3 <n3k00n3@protonmail.com>
2026-04-14 19:12:39 -0300

    change layout

--- voltmux.sh
+++ voltmux.sh
@@ -12,27 +12,30 @@ tmux kill-session -t $SESSION 2>/dev/null || true
 
 tmux new-session -d -s $SESSION
 
-tmux split-window -h -t $SESSION
+# Split right column (25% width)
+tmux split-window -h -p 25 -t $SESSION
+
+# Right column: reg on top (~60%), bp middle, bt bottom
+tmux select-pane -t $SESSION:0.1
 tmux send-keys -t $SESSION:0.1 "$VOLTRON view reg" Enter
 
+tmux split-window -v -p 30 -t $SESSION:0.1
+tmux send-keys -t $SESSION:0.2 "$VOLTRON view bp" Enter
+
+tmux split-window -v -p 50 -t $SESSION:0.2
+tmux send-keys -t $SESSION:0.3 "$VOLTRON view bt" Enter
+
+# Left column: disasm top, lldb middle, stack bottom
 tmux select-pane -t $SESSION:0.0
-tmux split-window -v -t $SESSION
+tmux split-window -v -p 50 -t $SESSION:0.0
 
 tmux select-pane -t $SESSION:0.1
-tmux split-window -v -t $SESSION
-
-tmux select-pane -t $SESSION:0.2
-tmux split-window -h -t $SESSION
-
-tmux select-pane -t $SESSION:0.0
-tmux split-window -h -t $SESSION
+tmux split-window -v -p 40 -t $SESSION:0.1
 
 tmux send-keys -t $SESSION:0.0 "$VOLTRON view disasm" Enter
-tmux send-keys -t $SESSION:0.1 "$VOLTRON view bp" Enter
-tmux send-keys -t $SESSION:0.2 "lldb $BINARY" Enter
-tmux send-keys -t $SESSION:0.3 "$VOLTRON view stack" Enter
-tmux send-keys -t $SESSION:0.4 "$VOLTRON view bt" Enter
+tmux send-keys -t $SESSION:0.1 "lldb $BINARY" Enter
+tmux send-keys -t $SESSION:0.2 "$VOLTRON view stack" Enter
 
-tmux select-pane -t $SESSION:0.2
+tmux select-pane -t $SESSION:0.1
 
 tmux attach -t $SESSION
